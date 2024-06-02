9a8a7e632bcf08f8a5eac6e83ae7e5eab6f9c410
AmIrLiUrFann2014 <166041039+AmIrLiUrFann2014@users.noreply.github.com>
2024-06-02 17:39:58 +0400

    homework for vagif teacher

---
+++ app.js
@@ -0,0 +1,20 @@
+//Task: 1.
+function printNumbers(n) {
+    for (let i = 1; i => n; i++) {
+        console.log(i);
+    }
+}
+
+const n = 10;
+printNumbers(n);
+//Task: 2.
+function sumArray {
+    let sum = 0;
+    for (let num of arr) {
+        sum += num;
+    }
+    return sum;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(sumArray(numbers));
